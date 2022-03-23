657c69f38410f18971fe682a0e50fdbcc9bff14f
mo550 <mohamed.elsayed1420@icloud.com>
2022-03-23 20:27:40 +0200

    first update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 let openToggle = document.getElementById("open-toggle");
 let closeToggle = document.getElementById("close-toggle");
 const navigation = document.querySelector(".navigation");
+let header = document.querySelector('.header');
 
 openToggle.addEventListener("click", () => {
   navigation.classList.add("active");
@@ -12,54 +13,93 @@ closeToggle.addEventListener("click", () => {
 
 // Close Navigation Menu When Click Escape Button
 document.onkeyup = function (e) {
-  // console.log(e);
   if (e.key === "Escape") {
     navigation.classList.remove("active");
   }
 };
+
+// Set background to header when scrolling
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 15) {
+    header.style.background = "rgba(51, 51, 51, 0.5)";
+  } else {
+    header.style.background = "transparent";
+  }
+})
 /** ------------------------------------------------------------------ **/
 
-/** Highlights NavLinks & NavNumber When Reaching Its Section **/
-const allSections = document.querySelectorAll('section');
-const navLinks = document.querySelectorAll(".navLinks li a");
-const allLis = document.querySelectorAll(".nav-num li a");
-const allSectionsLength = allSections.length;
+/** Switching betwen sections Functionality **/
+const allSections = document.querySelectorAll('.section');
+const allLis = document.querySelectorAll(".nav-num li");
 
-// Close Navigation Menu When Clicking Any NavLink
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    navigation.classList.remove("active");
-  });
-});
+allLis.forEach((li) => {
+  li.addEventListener('click', function() {
+    allLis.forEach((li) => {
+      li.classList.remove('active');
+    });
+      this.classList.add('active');
 
-// Highlighting Function
-window.addEventListener('scroll', () => {
-  let scrollPosition = document.documentElement.scrollTop;
+    // Remove show class first from allSections
+    allSections.forEach((section) => {
+      section.classList.remove('show');
+    });
 
-  allSections.forEach(section => {
-    if(scrollPosition >= section.offsetTop - 150 && scrollPosition < section.offsetTop + section.offsetHeight - 500) {
-      let currentId = section.getAttribute('id');
+    let liDataSet = li.dataset.id;
+    allSections.forEach((section) => {
+      let sectionId = section.id;
+      // Then add show class to setion if clicked li.id = section.id
+      if(liDataSet === sectionId) {
+        section.classList.add('show');
+      }
+    });
 
-      // Highlight for NavLinks
-      navLinks.forEach(el => {
-        if(el.getAttribute('href') === `#${currentId}`) {
-          el.classList.add('active');
-        } else {
-          el.classList.remove('active');
-        }
+    
+    /** Progress Animation **/
+    const progressBar = document.querySelectorAll(".progress-bar span");
+
+    if(liDataSet === "skills") {
+      progressBar.forEach((el) => {
+        const value = el.dataset.width;
+        el.style.width = `${value}%`;
       });
-
-      // Highlight for NavNumber
-      allLis.forEach(li => {
-        if(li.getAttribute('href') === `#${currentId}`) {
-          li.classList.add('active');
-        } else {
-          li.classList.remove('active');
-        } 
+    } else {
+      progressBar.forEach((el) => {
+        el.style.width = `0`;
       });
     }
   });
 });
+
+/* Highlighting Function */
+// ==> I Update This Functionality With Animation Sections (It's Here For Reference)
+
+// window.addEventListener('scroll', () => {
+//   let scrollPosition = document.documentElement.scrollTop;
+
+//   allSections.forEach(section => {
+//     if(scrollPosition >= section.offsetTop - 150 && scrollPosition < section.offsetTop + section.offsetHeight - 500) {
+//       let currentId = section.getAttribute('id');
+
+//       // Highlight for NavLinks
+//       navLinks.forEach(el => {
+//         if(el.getAttribute('href') === `#${currentId}`) {
+//           el.classList.add('active');
+//         } else {
+//           el.classList.remove('active');
+//         }
+//       });
+
+//       // Highlight for NavNumber
+//       allLis.forEach(li => {
+//         if(li.getAttribute('href') === `#${currentId}`) {
+//           li.classList.add('active');
+//         } else {
+//           li.classList.remove('active');
+//         } 
+//       });
+//     }
+//   });
+// });
 /** ------------------------------------------------------------------ **/
 
 /** Displaying Color Scheme **/
@@ -87,88 +127,78 @@ themeColors.forEach((color) => {
 });
 /** ------------------------------------------------------------------ **/
 
-/** Progress Animation **/
-const skillSection = document.getElementById("skills");
-const progressBar = document.querySelectorAll(".progress-bar span");
-const skilldcon = document.getElementById("progress-container")
-function showProgress() {
-  progressBar.forEach((el) => {
-    const value = el.dataset.width;
-    el.style.width = `${value}%`;
-  });
-}
-function hideProgress() {
-  progressBar.forEach((el) => {
-    el.style.width = `0`;
-  });
-}
-
-// Trigger The Show & Hide Function
-window.addEventListener("scroll", () => {
-  if(window.scrollY >= skillSection.offsetTop - 300) {
-    showProgress(); 
-  } else {
-    hideProgress();
-  }
-});
-/** ------------------------------------------------------------------ **/
-
 /** Menu Items Functionality **/
 const menu = [
   {
-    id: 1,
+    id: 9,
     category: "web",
     title: "Portfolio Website",
     link: "https://mo550.github.io/Personal-Portfolio/",
-    imgSrc: "img/project6",
+    repoLink: "https://github.com/mo550/Personal-Portfolio",
+    imgSrc: "img/project1",
   },
   {
-    id: 2,
+    id: 8,
     category: "graphic",
     title: "Knight Theme",
     link: "https://mo550.github.io/black-knight/",
+    repoLink: "https://github.com/mo550/black-knight",
     imgSrc: "img/project2",
   },
   {
-    id: 3,
+    id: 7,
     category: "branding",
     title: "Kasper",
     link: "https://mo550.github.io/kasper/",
+    repoLink: "https://github.com/mo550/kasper",
     imgSrc: "img/project3",
   },
   {
-    id: 4,
+    id: 6,
     category: "agency",
     title: "Healthy Food",
     link: "https://mo550.github.io/healthyfood/",
+    repoLink: "https://github.com/mo550/healthyfood",
     imgSrc: "img/project4",
   },
   {
     id: 5,
-    category: "web",
-    title: "FrontEnd Mentor",
-    link: "https://mo550.github.io/fylo/",
-    imgSrc: "img/project8",
-  },
-  {
-    id: 6,
-    category: "branding",
-    title: "FrontEnd Mentor",
-    link: "https://mo550.github.io/huddle/",
-    imgSrc: "img/project6",
-  },
-  {
-    id: 7,
-    category: "web",
-    title: "CV",
-    link: "https://mo550.github.io/My-CV/",
+    category: "agency",
+    title: "NFT",
+    link: "https://mo550.github.io/NFT/",
+    repoLink: "https://github.com/mo550/NFT",
     imgSrc: "img/project2",
   },
   {
-    id: 8,
+    id: 4,
+    category: "web",
+    title: "FrontEnd Mentor",
+    link: "https://mo550.github.io/fylo/",
+    repoLink: "https://github.com/mo550/fylo",
+    imgSrc: "img/project5",
+  },
+  {
+    id: 3,
+    category: "branding",
+    title: "FrontEnd Mentor",
+    link: "https://mo550.github.io/huddle/",
+    repoLink: "https://github.com/mo550/huddle",
+    imgSrc: "img/project6",
+  },
+  {
+    id: 2,
+    category: "web",
+    title: "CV",
+    link: "https://mo550.github.io/My-CV/",
+    repoLink: "https://github.com/mo550/My-CV",
+    imgSrc: "img/project7",
+  },
+  {
+    id: 1,
     category: "agency",
     title: "Leon Agency",
     link: "https://mo550.github.io/loantemplate/",
+    repoLink: "https://github.com/mo550/loantemplate",
     imgSrc: "img/project8",
   },
 ];
@@ -184,21 +214,35 @@ window.addEventListener('DOMContentLoaded', () => {
 function displayMenuItems(menuItems) {
   let displayMenu = menuItems
     .map((el) => {
-      return `<div class="card">
-      <h3 class="card-title">${el.title}</h3>
-      <picture>
-        <img src=${el.imgSrc} alt="Project${el.id}" 
-             style="width:100%; height:100%; object-fit: cover; object-position: center; border-radius: 15px;"
-        />
-      </picture>
-      <a href=${el.link} target="_blank" class="web-link">Visit The Website</a>
-    </div>`;
+      return `
+      <div class="card">
+        <div class="image">
+          <img src=${el.imgSrc} alt="Project${el.id}"/>
+        </div>
+        <div class="hover-items">
+          <div>
+            <h3>Project Source</h3>
+            <div class="icons">
+              <span class="icon preview" data-web=${el.link}>
+                <span class="icon-eye-solid"></span>
+              </span>
+              <a class="icon" href=${el.repoLink} target="_blank">
+                <span class="icon-code-branch-solid"></span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
     })
     .join("");
 
   // Appending Created MenuItems To Menu Container
   const menuContainer = document.querySelector(".menu-container");
   menuContainer.innerHTML = displayMenu;
+
+  // Triggering Preview Function
+  previewPopup();
 }
 
 // Displaying Menu Filter Btns
@@ -256,25 +300,70 @@ function displayFilterBtns() {
 }
 /** ------------------------------------------------------------------ **/
 
+/** Preview Popup Functionality **/
+function previewPopup() {
+  const previewPopup = document.querySelector('.preview-popup'),
+  closePreview = document.getElementById('close-preview'),
+  previewBtns = document.querySelectorAll('.preview'),
+  websiteFrame = document.getElementById('website-frame'),
+  frameLoader = document.getElementById('frame-loader'),
+  currentWebInd = document.querySelector('.current-web'),
+  totalWebPages = document.querySelector('.total-web');
+
+  // Set Websites Count Depending on previewBtns
+  totalWebPages.innerHTML = previewBtns.length;
+
+  // Close closePreview when clicking X button
+  closePreview.addEventListener('click', () => {
+    previewPopup.classList.remove('show');
+    // Reset iframe src to default
+    websiteFrame.src = "";
+  });
+
+  // Open previewPopup when clicking any of previewBtns
+  previewBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      previewPopup.classList.add('show');
+      // Set src of the iframe to previewBtn dataset (link)
+      websiteFrame.src = `${btn.dataset.web}`;
+      // Set current website index
+      currentWebInd.innerHTML = index + 1;
+      frameLoader.style.visibility = "visible";
+    });
+  });
+
+  // Hide preLoader when iframe finished loading
+  websiteFrame.addEventListener('load', () => {
+    frameLoader.style.visibility = "hidden";
+  });
+}
+/** ------------------------------------------------------------------ **/
+
+
 /** Slider Functionality **/
 const sliderTxt = [
   {
-    id: 1,
+    id: 5,
+    clientName: "Robert W.",
+    feedback: `Very Responsive. Good Communications.`,
+  },
+  {
+    id: 4,
     clientName: "Damon C.",
     feedback: `Mohamed did an exceptional job. He communicated perfectly and delivered all work quickly and exactly to specifications. He has excellent technical capabilities. Thanks Mohamed. We look forward to working with you in the future.`,
   },
   {
-    id: 2,
+    id: 3,
     clientName: "Nikhil P.",
     feedback: `It was a pleasure to work with Mohamed. He is knowledgeable and always helpful. Really easy to work with, a wonderful experience!`,
   },
   {
-    id: 3,
+    id: 2,
     clientName: "Nikhil P.",
     feedback: `A real pleasure working with this freelancer. Understood requirements clearly and worked really quickly in producing exactly what we wanted. Attention to detail and was always available for any changes.`,
   },
   {
-    id: 4,
+    id: 1,
     clientName: "Nada A.",
     feedback: `I'm happy to work with you, you have a high skill in development, for sure I will work with you again if I need anything related to this subject :)`,
   },
@@ -290,7 +379,7 @@ let sliderContainer = document.querySelector(".slider-container");
     .map((slide) => {
       return `
     <div class="slide">
-      <span class="quote"><i class="fas fa-quote-right"></i></span>
+      <span class="quote"><span class="icon-quote-right-solid"></span></i></span>
       <img class="client-img" src="img/man.png" alt="Client">
       <div class="stars">
         <span class="icon-star-solid"></span>
@@ -376,40 +465,5 @@ let sliderContainer = document.querySelector(".slider-container");
 
 /** Loader Functionality **/
 const loader = document.querySelector('.loader');
-window.addEventListener('load', () => {
-  loader.style.opacity = '0';
-  loader.style.display = 'none';
-});
-/** ------------------------------------------------------------------ **/
-
-/** ScrollToTopFunctionality **/
-const scrollToTop = document.getElementById('scrollToTop');
-
-window.addEventListener('scroll', () => {
-  if(window.scrollY > 700) {
-    scrollToTop.classList.add('active');
-  }
-  else {
-    scrollToTop.classList.remove('active');
-  }
-});
-
-scrollToTop.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth',
-  });
-});
-/** ------------------------------------------------------------------ **/
-
-/** Initialize AOS Library **/
-(function() {
-  AOS.init({
-    duration: 700,
-    offset: 200,
-    once: true
-  });
-  window.addEventListener('load', AOS.refresh);
-}());
+window.onload = () => loader.style.display = 'none';
 /** ------------------------------------------------------------------ **/
