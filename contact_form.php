2a5302517343a620eebf8bee671cccf6cd9d1e0d
Mohamed Elsayed <mohamed.elsayed1420@icloud.com>
2023-02-12 14:27:02 +0200

    up

--- contact_form.php
+++ contact_form.php
@@ -1,37 +1,25 @@
 <?php
-if(isset($_POST["send_a_msg"])){
-  $name = $_POST["name"];  
-  $email = $_POST["email"];  
-  $message = $_POST["message"];  
-  
-$to = "mohamed.elsayed7272@gmail.com";
+if(!empty($_POST["send_a_msg"])) {
+  $userName = $_POST["userName"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+  $toEmail = "mohamed.elsayed7272@gmail.com";
 
-$subject = "You have Received New Message - Mohamed Elsayed ";
+  $message = '
+    <html>
+    <head>
+    <title>Mohamed Elsayed - Contact Us</title>
+    </head>
+    <body>
+      <p> You have received new message </p>
+      <div>
+      <p> <b>First Name</b> : '.$userName.' </p>
+      <p> <b>Last Name</b> : '.$email.' </p>
+      <p> <b>Email</b> : '.$message.' </p>
+    </body>
+    </html>
+    ';
 
-$message = '
-<html>
-<head>
-<title>Mohamed Elsayed - Contact Us</title>
-</head>
-<body>
-<p> You have received new message </p>
-<div>
-<p> <b>First Name</b> : '.$name.' </p>
-<p> <b>Email</b> : '.$email.' </p>
-<p> <b>Message</b> : '.$message.' </p>
-</div>
-</body>
-</html>
-';
-
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-$headers .= 'From: <MohamedElsayed@example.com>' . "\r\n";
-//$headers .= 'Cc: myboss@example.com' . "\r\n";
-
-mail($to,$subject,$message,$headers);  
-header("Location: https://mohamedelsayed.netlify.app/");   
+    mail($toEmail, $userName, $message);
 }
+?>
